3dbcac87795bf759c8761e9244698f708e1bc023
Jacob Pope <Jacob_Pope4@baylor.edu>
2023-03-26 22:31:33 -0500

    Added potential for loops for headers

--- sitka5.py
+++ sitka5.py
@@ -11,10 +11,10 @@ sitka_csvfile = csv.reader(sitka_infile, delimiter=",")
 death_header_row = next(death_csvfile)  # eliminates the first line
 sitka_header_row = next(sitka_csvfile)
 
-print(type(header_row))
+# print(type(header_row))
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+# for index, column_header in enumerate(header_row):
+#    print(index, column_header)
 
 death_highs = []  # y axis
 death_lows = []
@@ -28,6 +28,26 @@ sitka_dates = []  # x axis
 # columns. Use the header row to determine the indexes for these values, so your program can work
 # for Sitka or Death Valley. Use the station name to automatically generate an appropriate title
 # for your graph as well.
+for index, column_header in enumerate(death_header_row):
+    if column_header == "TMAX":
+        death_max_index = index
+    elif column_header == "TMIN":
+        death_min_index = index
+    elif column_header == "DATE":
+        death_date_index = index
+    elif column_header == "NAME":
+        death_name_index = index
+
+for index, column_header in enumerate(sitka_header_row):
+    if column_header == "TMAX":
+        sitka_max_index = index
+    elif column_header == "TMIN":
+        sitka_min_index = index
+    elif column_header == "DATE":
+        sitka_date_index = index
+    elif column_header == "NAME":
+        sitka_name_index = index
+
 
 for row in csvfile:
     highs.append(int(row[5]))  #
